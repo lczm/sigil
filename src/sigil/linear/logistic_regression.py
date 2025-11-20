8d544e017db7630a48647b0ead15cf21c000a690
lczm <zemingchua@gmail.com>
2025-11-20 20:13:24 +0800

    chore: add build() to the models (#6)

--- src/sigil/linear/logistic_regression.py
+++ src/sigil/linear/logistic_regression.py
@@ -14,10 +14,22 @@ class LogisticRegression(BaseModel):
         self.n_features: int
         self.n_samples: int
 
-    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+    def build(self, X: np.ndarray, **kwargs) -> None:
         self.n_samples, self.n_features = X.shape
-        self.weights = np.zeros(self.n_features)
-        self.bias = 0
+
+        if "initial_weights" in kwargs:
+            self.weights = kwargs["initial_weights"]
+        else:
+            self.weights = np.zeros(self.n_features)
+
+        if "initial_bias" in kwargs:
+            self.bias = kwargs["initial_bias"]
+        else:
+            self.bias = 0.0
+
+    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+        if self.weights is None or self.bias is None:
+            self.build(X)
 
         for _ in tqdm(range(self.n_iterations)):
             self.step(X, y)
--- src/sigil/models.py
+++ src/sigil/models.py
@@ -4,16 +4,29 @@ import numpy as np
 
 class BaseModel(ABC):
     @abstractmethod
+    def build(self, X: np.ndarray, **kwargs) -> None:
+        """
+        Method to initialize internal model state based on input shape
+        """
+        pass
+
+    @abstractmethod
     def fit(self, X: np.ndarray, y: np.ndarray) -> None:
-        """Method to train the regression model."""
+        """
+        Method to train the regression model.
+        """
         pass
 
     @abstractmethod
     def predict(self, input_data: np.ndarray) -> np.ndarray:
-        """Method to predict target values for given input data."""
+        """
+        Method to predict target values for given input data.
+        """
         pass
 
     @abstractmethod
     def step(self, X: np.ndarray, y: np.ndarray) -> None:
-        """Method to perform a step in the training process."""
+        """
+        Method to perform an iterative step in the training process.
+        """
         pass
